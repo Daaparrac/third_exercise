8291d216b3abdf22a7585b9af18de51f6f6e9573
Daniel parra <danielparrac26@gmail.com>
2020-08-02 15:15:31 -0500

    proyecto finalizado

--- db/script_table.sql
+++ db/script_table.sql
@@ -5,16 +5,23 @@ CREATE TABLE Employee
     funcion varchar(200) NOT NULL
 );
 
-
+create table Employees
+(
+    id_employee int(11) not null primary key,
+    fullname varchar(50),
+    funcion varchar(50),
+    id_jefe int(11),
+    constraint jefe_fk foreign key (id_jefe) references personal (id_personal)
+);
 
 INSERT INTO Employee
     (fullname,funcion)
 VALUES
-    (1, 'Empleado1', 'funcion'),
-    (2, 'Empleado2', 'funcion'),
-    (3, 'Empleado3', 'funcion'),
-    (4, 'Empleado4', 'funcion'),
-    (5, 'Empleado5', 'funcion'),
-    (6, 'Empleado6', 'funcion'),
-    (7, 'Empleado7', 'funcion'),
-    (8, 'Empleado8', 'funcion');
+    (1, 'Empleado1', 'funcion', 1),
+    (2, 'Empleado2', 'funcion', 2),
+    (3, 'Empleado3', 'funcion', 1),
+    (4, 'Empleado4', 'funcion', 3),
+    (5, 'Empleado5', 'funcion', 1),
+    (6, 'Empleado6', 'funcion', 1),
+    (7, 'Empleado7', 'funcion', null),
+    (8, 'Empleado8', 'funcion', 1);
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require("express");
+var CORS = require("cors");
 var app = express();
 var bodyParser = require("body-parser");
 var mysql = require("mysql");
-
+app.use(CORS({ origin: true }));
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -37,60 +38,36 @@ app.get("/employees", function (req, res) {
   });
 });
 
-// Retrieve user with id
-app.get("/user/:fullname", function (req, res) {
-  let employee_name = req.params.fullname;
-
-  if (!employee_name) {
-    return res.status(400).send({
-      error: true,
-      message: "Please provide fullname for example 'Empleado1'",
-    });
-  }
-
-  dbConn.query("SELECT * FROM Employees ", employee_name, function (
-    error,
-    results,
-    fields
-  ) {
-    if (error) throw error;
-    return res.send({
-      error: false,
-      data: results[0],
-      message: "Employees list.",
-    });
-  });
-});
-
 // Add a new employee
 app.post("/employee", function (req, res) {
-  let employee = req.body.employee;
-  console.log(employee);
-  if (!employee) {
-    return res
-      .status(400)
-      .send({ error: true, message: "Please provide employee" });
-  }
-
-  console.log(employee);
+  let id_employee = req.body.id_employee;
+  let fullname = req.body.fullname;
+  let funcion = req.body.funcion;
+  let id_boss = req.body.id_boss;
 
   dbConn.query(
-    "INSERT INTO Employees (id_employee,fullname,funcion,id_boss) VALUES ",
-    { employee: employee },
+    `INSERT INTO Employees (id_employee,fullname,funcion,id_boss) Values (?, ?, ?, ?)`,
+    [id_employee, fullname, funcion, id_boss],
     function (error, results, fields) {
       if (error) throw error;
       return res.send({
         error: false,
         data: results,
-        message: "New employee has been created successfully.",
+        message: "successfully added",
       });
     }
   );
 });
 
+app.delete("/deleteEmp/:id", function (req, res) {
+  let id = req.params.id;
+  console.log(id);
+  dbConn.query("DELETE FROM Employees WHERE id_employee = ?", id);
+});
+
 // set port
 app.listen(33068, function () {
-  console.log("Node app is running on port 3000");
+  console.log("Node app is running on port 33068");
 });
 
 module.exports = app;
